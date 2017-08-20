952cd265da9183eaee74e5d8f3cb1ad3e9e83c8f
sinlov <sinlovgmppt@gmail.com>
2017-08-21 01:00:23 +0800

    add HOME Path utils

--- cli/cmdPath.go
+++ cli/cmdPath.go
@@ -6,6 +6,10 @@ import (
 	"path/filepath"
 	"strings"
 	"github.com/sinlov/golang_utils/jstring"
+	"runtime"
+	"os/user"
+	"bytes"
+	"errors"
 )
 
 func CommandPath() string {
@@ -29,4 +33,60 @@ func CurrentDirectory() string {
 	} else {
 		return strings.Replace(dir, "/", "/", -1)
 	}
+}
+
+// Home returns the home directory for the executing user.
+//
+// This uses an OS-specific method for discovering the home directory.
+// An error is returned if a home directory cannot be detected.
+func Home() (string, error) {
+	userCur, err := user.Current()
+	if nil == err {
+		return userCur.HomeDir, nil
+	}
+
+	// cross compile support
+
+	if "windows" == runtime.GOOS {
+		return homeWindows()
+	}
+
+	// Unix-like system, so just assume Unix
+	return homeUnix()
+}
+
+func homeUnix() (string, error) {
+	// First prefer the HOME environmental variable
+	if home := os.Getenv("HOME"); home != "" {
+		return home, nil
+	}
+
+	// If that fails, try the shell
+	var stdout bytes.Buffer
+	cmd := exec.Command("sh", "-c", "eval echo ~$USER")
+	cmd.Stdout = &stdout
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	result := strings.TrimSpace(stdout.String())
+	if result == "" {
+		return "", errors.New("blank output when reading home directory")
+	}
+
+	return result, nil
+}
+
+func homeWindows() (string, error) {
+	drive := os.Getenv("HOMEDRIVE")
+	path := os.Getenv("HOMEPATH")
+	home := drive + path
+	if drive == "" || path == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	if home == "" {
+		return "", errors.New("HOMEDRIVE, HOMEPATH, and USERPROFILE are blank")
+	}
+
+	return home, nil
 }
--- cli/cmdPath_test.go
+++ cli/cmdPath_test.go
@@ -1,8 +1,8 @@
 package cli
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestCommandPath(t *testing.T) {
@@ -67,6 +67,81 @@ func TestCurrentDirectory(t *testing.T) {
 			if got := CurrentDirectory(); got != tt.want {
 				fmt.Printf("got: %v\n", got)
 				//t.Errorf("CurrentDirectory() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHome(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:"test Home",
+			want:"/Users/sinlov",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Home()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Home() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Home() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_homeUnix(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:"Test_homeUnix",
+			want:"/Users/sinlov",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := homeUnix()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("homeUnix() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("homeUnix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_homeWindows(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:"Test_homeUnix",
+			want:"C:\\User\\sinlov",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := homeWindows()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("homeWindows() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("homeWindows() = %v, want %v", got, tt.want)
 			}
 		})
 	}
