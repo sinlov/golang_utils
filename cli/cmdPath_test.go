f08947b8d2cca653d713a3ccd9b4091206a50175
sinlov <sinlovgmppt@gmail.com>
2018-02-20 10:27:13 +0800

    let cmdPath full test convey

--- cli/cmdPath_test.go
+++ cli/cmdPath_test.go
@@ -1,157 +1,125 @@
 package cli
 
 import (
-	"fmt"
 	"testing"
+	"github.com/smartystreets/goconvey/convey"
+	"os/exec"
+	"os"
+	"os/user"
 )
 
 func TestCommandPath(t *testing.T) {
-	tests := []struct {
-		name string
-		want string
-	}{
-		{
-			name: "CommandPath",
-			want: "",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CommandPath(); got != tt.want {
-				fmt.Printf("got: %v\n", got)
-				//t.Errorf("CommandPath() = %v, want %v", got, tt.want)
-			}
+	convey.Convey("mock TestCommandPath", t, func() {
+		// mock
+		want, _ := exec.LookPath(os.Args[0])
+		convey.Convey("do TestCommandPath", func() {
+			// do
+			got := CommandPath()
+			convey.Convey("verify TestCommandPath", func() {
+				// verify
+				convey.So(got, convey.ShouldEqual, want)
+			})
 		})
-	}
-}
-
-func TestParentDirectory(t *testing.T) {
-	type args struct {
-		directory string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "ParentDirectory",
-			args: args{
-				directory: CurrentDirectory(),
-			},
-			want: "",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ParentDirectory(tt.args.directory); got != tt.want {
-				fmt.Printf("got: %v\n", got)
-				//t.Errorf("ParentDirectory() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	})
 }
 
 func TestCurrentDirectory(t *testing.T) {
-	tests := []struct {
-		name string
-		want string
-	}{
-		{
-			name: "CurrentDirectory",
-			want: "",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CurrentDirectory(); got != tt.want {
-				fmt.Printf("got: %v\n", got)
-				//t.Errorf("CurrentDirectory() = %v, want %v", got, tt.want)
-			}
+	convey.Convey("mock TestCurrentDirectory", t, func() {
+		// mock
+		want := ""
+		convey.Convey("do TestCurrentDirectory", func() {
+			// do
+			got := CurrentDirectory();
+			convey.Convey("verify TestCurrentDirectory", func() {
+				// verify
+				t.Skipf("%s %s got %s", t.Name(), "cuz got Directory not for test just skip", got)
+				convey.So(got, convey.ShouldEqual, want)
+			})
 		})
-	}
+	})
+}
+
+func TestParentDirectory(t *testing.T) {
+	convey.Convey("mock TestParentDirectory", t, func() {
+		// mock
+		directory := CurrentDirectory()
+		want := ""
+		convey.Convey("do TestParentDirectory", func() {
+			// do
+			got := ParentDirectory(directory)
+			convey.Convey("verify TestParentDirectory", func() {
+				// verify
+				t.Skipf("%s %s got %s", t.Name(), "cuz got Directory not for test just skip", got)
+				convey.So(got, convey.ShouldEqual, want)
+			})
+		})
+	})
 }
 
 func TestHome(t *testing.T) {
-	tests := []struct {
-		name    string
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "test Home",
-			want: "/Users/sinlov",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if IsSysWindows() {
-				t.Skipf("%s %s", tt.name, "now system windows so pass homeUnix!")
-			}
+	convey.Convey("mock TestHome", t, func() {
+		// mock
+		userCur, _ := user.Current()
+		want := userCur.HomeDir
+		convey.Convey("do TestHome", func() {
+			// do
 			got, err := Home()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Home() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("Home() = %v, want %v", got, tt.want)
-			}
+			convey.Convey("verify TestHome", func() {
+				// verify
+				if err != nil {
+					t.Errorf("%s got User Home error, %s", t.Name(), err)
+				} else {
+					convey.So(got, convey.ShouldEqual, want)
+				}
+
+			})
 		})
-	}
+	})
 }
 
-func Test_homeUnix(t *testing.T) {
-	tests := []struct {
-		name    string
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "Test_homeUnix",
-			want: "/Users/sinlov",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if IsSysWindows() {
-				t.Skipf("%s %s", tt.name, "now system windows so pass homeUnix!")
-			}
+func TestHomeUnix(t *testing.T) {
+	convey.Convey("mock TestHomeUnix", t, func() {
+		// mock
+		userCur, _ := user.Current()
+		want := userCur.HomeDir
+		convey.Convey("do TestHomeUnix", func() {
+			// do
 			got, err := homeUnix()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("homeUnix() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("homeUnix() = %v, want %v", got, tt.want)
-			}
+			convey.Convey("verify TestHomeUnix", func() {
+				// verify
+				if err != nil {
+					t.Errorf("%s got User Home by homeUnix() error, %s", t.Name(), err)
+				} else {
+					if IsSysWindows() {
+						t.Skipf("%s %s", t.Name(), "now system windows so pass homeUnix!")
+					} else {
+						convey.So(got, convey.ShouldEqual, want)
+					}
+				}
+			})
 		})
-	}
+	})
 }
 
-func Test_homeWindows(t *testing.T) {
-	tests := []struct {
-		name    string
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "Test_homeUnix",
-			want: "C:\\User\\sinlov",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+func TestHomeWindows(t *testing.T) {
+	convey.Convey("mock TestHomeWindows", t, func() {
+		// mock
+		userCur, _ := user.Current()
+		want := userCur.HomeDir
+		convey.Convey("do TestHomeWindows", func() {
+			// do
 			if !IsSysWindows() {
-				t.Skipf("%s %s", tt.name, "not windows system so pass!")
+				t.Skipf("%s %s", t.Name(), "not windows system so pass!")
 			}
 			got, err := homeWindows()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("homeWindows() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("homeWindows() = %v, want %v", got, tt.want)
-			}
+			convey.Convey("verify TestHomeWindows", func() {
+				// verify
+				if err != nil {
+					t.Errorf("%s got User Home by homeWindows() error, %s", t.Name(), err)
+				} else {
+					convey.So(got, convey.ShouldEqual, want)
+				}
+			})
 		})
-	}
+	})
 }
