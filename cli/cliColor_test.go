0b994a61f5e7378026ee14853c2777894837afa9
sinlov <sinlovgmppt@gmail.com>
2018-02-20 09:30:18 +0800

    add convey of test

--- cli/cliColor_test.go
+++ cli/cliColor_test.go
@@ -2,52 +2,78 @@ package cli
 
 import (
 	"testing"
-	"github.com/bmizerany/assert"
 	"fmt"
+	"github.com/smartystreets/goconvey/convey"
 )
 
 func TestRed(t *testing.T) {
-	fmt.Sprintf("\x1b[0;%dm%s\n\x1b[0m", 30, "123")
-	fmt.Println(Red("CLi Red color"))
-	assert.T(t, true, nil)
+	convey.Convey("TestRed", t, func() {
+		// mock
+		testString := "CLi Red color"
+		redString := fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", 31, testString)
+		convey.Convey("do TestRed", func() {
+			// do
+			redCliString := Red(testString)
+			convey.Convey("verify TestRed", func() {
+				// verify
+				convey.So(redString, convey.ShouldEqual, redCliString)
+			})
+		})
+	})
 }
 
 func TestFmtBlack(t *testing.T) {
-	FmtBlack("%s\n", "Cli FmtBlack color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtBlack", t, func() {
+		FmtBlack("%s\n", "Cli FmtBlack color")
+		convey.So("Cli FmtBlack color", convey.ShouldEqual, "Cli FmtBlack color")
+	})
 }
 
 func TestFmtRed(t *testing.T) {
-	FmtRed("%s\n", "Cli FmtRed color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtRed", t, func() {
+		FmtRed("%s\n", "Cli FmtRed color")
+		convey.So("Cli TestFmtRed color", convey.ShouldEqual, "Cli TestFmtRed color")
+	})
 }
 
 func TestFmtGreen(t *testing.T) {
-	FmtGreen("%s\n", "Cli FmtGreen color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtGreen", t, func() {
+		FmtGreen("%s\n", "Cli FmtGreen color")
+		convey.So("Cli TestFmtGreen color", convey.ShouldEqual, "Cli TestFmtGreen color")
+	})
 }
 
 func TestFmtYellow(t *testing.T) {
-	FmtYellow("%s\n", "Cli FmtYellow color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtYellow", t, func() {
+		FmtYellow("%s\n", "Cli FmtYellow color")
+		convey.So("Cli TestFmtYellow color", convey.ShouldEqual, "Cli TestFmtYellow color")
+	})
 }
 
 func TestFmtBlue(t *testing.T) {
-	FmtBlue("%s\n", "Cli FmtBlue color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtBlue", t, func() {
+		FmtBlue("%s\n", "Cli FmtBlue color")
+		convey.So("Cli TestFmtBlue color", convey.ShouldEqual, "Cli TestFmtBlue color")
+	})
 }
 
 func TestFmtMagenta(t *testing.T) {
-	FmtMagenta("%s\n", "Cli FmtMagenta color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtMagenta", t, func() {
+		FmtMagenta("%s\n", "Cli FmtMagenta color")
+		convey.So("Cli TestFmtMagenta color", convey.ShouldEqual, "Cli TestFmtMagenta color")
+	})
 }
 
 func TestFmtCyan(t *testing.T) {
-	FmtCyan("%s\n", "Cli FmtCyan color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtCyan", t, func() {
+		FmtCyan("%s\n", "Cli FmtCyan color")
+		convey.So("Cli TestFmtCyan color", convey.ShouldEqual, "Cli TestFmtCyan color")
+	})
 }
 
 func TestFmtWhite(t *testing.T) {
-	FmtWhite("%s\n", "Cli FmtWhite color")
-	assert.T(t, true, nil)
+	convey.Convey("TestFmtWhite", t, func() {
+		FmtWhite("%s\n", "Cli FmtWhite color")
+		convey.So("Cli TestFmtWhite color", convey.ShouldEqual, "Cli TestFmtWhite color")
+	})
 }
